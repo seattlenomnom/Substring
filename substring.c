dcab46757343abb406ed8fb86691558c27f87de4
seattlenomnom <mark.crapser@gmail.com>
2022-08-09 13:36:34 -0700

    Can enter the original string

--- substring.c
+++ substring.c
@@ -2,13 +2,17 @@
  * The source file for substring, a program the gets a string from the user and
  * removes a substring using parameters provided by the user.
  *
+ *
+ * 08/09/2022 test fgets result. puts used to test fgets. Printf used with
+ * fgets gives me a leading blank on printing to screen.
+ *
 */
 
 #include <stdio.h>
-
+#include <string.h>
 
 #define MAX_LENGTH 80
-
+const char string_prompt[] = "Enter string: ";
 
 
 
@@ -26,9 +30,8 @@ int main(int argc, char *argv[]) {
     char string[MAX_LENGTH + 1];
     /* input */
         /* get a string from the user */
-    printf("Enter a string: ");
+    puts(string_prompt);
     fgets(string, MAX_LENGTH, stdin);
-
         /* get the parameters of the substring from the user */
 
 
